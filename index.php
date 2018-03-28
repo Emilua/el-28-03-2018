3586a7e39124e781135aa8a3baef8efdc25124d7
somebody <somebody@smd.com>
2018-03-28 19:22:32 +0300

    создал объект на основании класса.

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php
-
+spl_autoload_register(function($class) {
+    $classFileName = 'vendor' . DIRECTORY_SEPARATOR . $class . '.php';
+    if (file_exists($classFileName)) {
+        include_once $classFileName;
+        return true;
+    }
+    return false;
+});
+$testObj=new Test();
+$testObj->name='Vasya';
+var_dump($testObj);
+echo($testObj->name);
