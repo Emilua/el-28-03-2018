139d450ec7622e805370d8303ac6bece844f6732
somebody <somebody@smd.com>
2018-03-28 20:05:50 +0300

    создал класс Human

---
+++ vendor/Human.php
@@ -0,0 +1,56 @@
+<?php
+
+class Human
+{/**
+ *
+ * @var srt 
+ */
+    protected $name;
+    /**
+     *
+     * @var str
+     */
+    protected $surname;
+    
+    /**
+     *
+     * @var int
+     */
+    protected $age;
+    
+    /**
+     *
+     * @var str
+     */
+    protected $address;
+    
+    
+    /**
+     * 
+     * @param string$name
+     * @param string$surname
+     * @param int $age
+     * @param string $address
+     */
+    public function __construct($name, $surname, $age, $address)
+    {
+        $this->name=$name;
+        $this->surname=$surname;
+        $this->age=$age;
+        $this->address=$address;
+    }
+    
+    /**
+     * 
+     * @return string
+     */
+    public function __toString()
+    {
+        $str= '';
+        $str.= 'Имя: '. $this->name;
+        $str.= 'Фамилия: '. $this->surname;
+        $str.= 'Возраст: '. $this->age;
+        $str.= 'Адрес: '. $this->address;
+        return $str;
+    }
+}
